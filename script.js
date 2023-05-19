82a887eca4b226b0c525d7d8d9be7653acb32fc4
Kartikey Singh <92157475+Kartikey06dk@users.noreply.github.com>
2023-05-19 15:47:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
+const lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M'
+};
 
-const express = require('express');
-const path = require('path');
+function rot13(encodedStr){
+  const codeArr = encodedStr.split("");  // String to Array
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+  // your code here
+  for(var s in encodedStr){
+    decodedArr.push(String.fromCharCode(97 + ((encodedStr.charCodeAt(s) - 97 + 13)%26)));
+    // console.log(encodedStr.charCodeAt(s) );
+  }
+  // Only change code above this line
 
-const app = express();
+  return decodedArr.join(""); // Array to String
+}
 
-app.use(express.static(__dirname))
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
+
+module.exports = rot13;
